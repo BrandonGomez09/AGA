54b592cb21b221164d24c3975f2b2f4fad5dc50e
BrandonGomez09 <231205@ids.upchiapas.edu.mx>
2023-11-22 21:18:59 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-500">
+      <h1 className="text-3xl font-bold underline bg-red-400">
         Hello world!
       </h1>
     </>
